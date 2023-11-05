7f99f362854a28aedb226021a572bb4385117f8f
@bryant <>
2023-11-05 14:43:50 +0800

    Github

---
+++ src/quiz52021002/QUIZ52021002.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz52021002;
+
+/**
+ *
+ * @author HP VICTUS
+ */
+public class QUIZ52021002 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new MenuMain();
+    }
+    
+}
+     
